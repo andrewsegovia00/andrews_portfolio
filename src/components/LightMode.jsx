906a62e3acd79ea0e0fa37918840d3710c3546e2
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-30 20:44:52 -0700

    Added bootstrap styling to page

--- src/components/LightMode.jsx
+++ src/components/LightMode.jsx
@@ -3,9 +3,6 @@ import '../style/lightMode.css'
 function LightMode({ toggleMode }) {
   return (
     <>
-    <head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-    </head>
       <div className="video-container">
       <video autoPlay loop muted playsInline>
         <source src="../src/assets/videos/light.mp4" id="video" type="video/mp4" />
